52ca078bb34b875c493bd8d2731ca566d88b8d0c
MatrixSenpai <math.matrix@icloud.com>
2018-06-10 00:27:40 -0500

    Adding FontBlaster (user needs to add the appropriate fonts)

--- Syndra/AppDelegate.swift
+++ Syndra/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MMDrawerController
+import FontBlaster
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var viewController: MMDrawerController?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FontBlaster.blast()
         window = UIWindow(frame: UIScreen.main.bounds)
         
         let leftController = menuTableViewController()
